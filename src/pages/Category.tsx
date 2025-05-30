8d158ed12d1b83253f1d86b2637a83acbce17aad
aiskairr <aminhansss2222@gmail.com>
2025-05-30 17:34:47 +0600

    finished

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -257,7 +257,7 @@ const Category = () => {
               alt={categoryNames[categorySlug]} 
               className="w-full h-full object-cover"
             />
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+            <div className="absolute inset-0 bg-opacity-90 flex items-center justify-center" style={{background: "rgb(227 6 19 / var(--tw-bg-opacity, 1))"}}>
               <h1 className="text-3xl font-bold text-white">{categoryNames[categorySlug] || 'Товары'}</h1>
             </div>
           </div>
